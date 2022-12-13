3bf0594862ae8dfa37b8dec47cb57d0aa47b7bbd
bob-skywalker <q463250938@gmail.com>
2022-12-13 12:36:11 -0800

    answer logic implemented : user will only be able to proceed when they get the question correctly , fixed out of bound issue when incrementing questionNum

--- quizller-ios/ViewController.swift
+++ quizller-ios/ViewController.swift
@@ -13,13 +13,35 @@ class ViewController: UIViewController {
     @IBOutlet weak var TrueButton: UIButton!
     @IBOutlet weak var FalseButton: UIButton!
     
+    let quiz = [
+        ["Four + Two is equal to Six.", "True"],
+        ["Five - Three is greater than One", "True"],
+        ["Three + Eight is less than Ten", "True"]
+    ]
+    
+    var questionNum = 0
+    
     @IBAction func answerButtonPressed(_ sender: UIButton) {
+        let userAnswer = sender.currentTitle
+        let actualAnswer = quiz[questionNum][1]
         
+        if userAnswer == actualAnswer && questionNum < quiz.count - 1 {
+            questionNum += 1
+            updateUI()
+        } else if userAnswer == actualAnswer && questionNum == quiz.count - 1 {
+            questionNum = 0
+            updateUI()
+        }
+    }
+    
+    func updateUI(){
+        questionLabel.text = quiz[questionNum][0]
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        updateUI()
     }
 
 
