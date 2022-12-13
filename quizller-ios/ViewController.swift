bd0f4fd83ac9fec51e88bedfad8d4aa9ec2d82c5
bob-skywalker <q463250938@gmail.com>
2022-12-13 11:55:12 -0800

    IBOutlets & IBAction buttons created and linked

--- quizller-ios/ViewController.swift
+++ quizller-ios/ViewController.swift
@@ -8,7 +8,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var TrueButton: UIButton!
+    @IBOutlet weak var FalseButton: UIButton!
+    
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
